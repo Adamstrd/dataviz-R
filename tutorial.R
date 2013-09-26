8e92436993f361bc7644e8125a2ba265cdf5489f
Adamstrd <theresaadams@man-97-107.reshall.berkeley.edu>
2013-09-25 23:03:55 -0700

    Bar new

---
+++ tutorial.R
@@ -0,0 +1,13 @@
+class(prices)
+dim(prices)
+head(prices)
+head(prices, n=20)
+names(prices)
+prices$X2013
+prices$X2013[1:10]
+class(prices$X2013)
+?head
+prices[1,]
+prices[,1]
+prices[,c("Network")]
+prices[1:10,c("Network")]
